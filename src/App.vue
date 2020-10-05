5e5bc604187a58561301881e47421aa285708424
jassy1995 <babatundejoseph85@gmail.com>
2020-10-05 07:11:01 +0100

    this is my firt commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+         <h1 class="text-center">The Super Quiz</h1>
+       </div>
+    </div>
+    <hr>
+    <div class="row justify-content-center">
+       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+         <transition name="flip" mode="out-in">
+          <component :is="mode" @answered="answered($event)" @confirmed="mode= 'Question'"></component>
+         </transition>
+       </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Question  from './components/question';
+import Answer from './components/answer';
+export default {
+  name: 'App',
+  data(){
+    return{
+      mode:'Question'
+    }
+  },
+  components: {Question,Answer },
+  methods:{
+    answered(isCorrect){
+      if(isCorrect){
+        this.mode = 'Answer';
+      }
+      else{
+        this.mode = 'Question';
+        alert('you are wrong')
+      }
+
+    }
+  }
+    
+ 
+}
+</script>
+
+<style>
+.flip-enter-active{
+ animation:flip-in 0.5s ease-out forwards;
+}
+.flip-leave-active{
+   animation:flip-out 0.5s ease-out forwards;
+}
+@keyframes flip-out {
+  from{
+    transform: rotateY(0deg);
+  }
+  to{
+    transform: rotateY(90deg);
+  }
+  
+}
+@keyframes flip-in {
+  from{
+    transform: rotateY(90deg);
+  }
+  to{
+    transform: rotateY(0deg);
+  }
+  
+}
+</style>
---
+++ src/components/answer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="alert alert-success text-center">
+         <h1 class="text-center">That's Correct</h1>
+         <br>
+         <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Answer',
+  
+  methods: {
+    onNextQuestion(){
+        this.$emit('confirmed');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
